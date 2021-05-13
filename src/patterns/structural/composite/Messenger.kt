5398e3b2eadc1b001b553bb056769f1c0514b13e
vlas <vlas.artanov@privatbank.ua>
2021-05-13 11:39:55 +0300

    small fix

--- src/patterns/structural/composite/Messenger.kt
+++ src/patterns/structural/composite/Messenger.kt
@@ -1,21 +1,16 @@
 package patterns.structural.composite
 
-import com.sun.tools.javac.util.List
-import patterns.structural.composite.LetterComposite
-import patterns.structural.composite.Word
-import patterns.structural.composite.Sentence
-
 class Messenger {
     fun messageFromAndroid(): LetterComposite {
-        val words: List<Word> = List.of(
+        val words: List<Word> = listOf(
             Word('G', 'o', 'o', 'g', 'l', 'e'),
-            Word('I','/', 'O')
+            Word('I', '/', 'O')
         )
         return Sentence(words)
     }
 
     fun messageFromIos(): LetterComposite {
-        val words: List<Word> = List.of(
+        val words: List<Word> = listOf(
             Word('I', 't', '\'', 's'),
             Word('a', 'm', 'a', 'z', 'i', 'n', 'g')
         )
